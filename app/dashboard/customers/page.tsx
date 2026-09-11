7c4ed1992dfffed8a314f09d284a7bbdb8735b23
Martin Najle <martin.najle@gmail.com>
2026-09-11 14:05:16 -0300

    Adds pages and layout features

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+const CustomersPage = () => {
+  return (
+    <div>
+      <h1>Customers Page</h1>
+      <p>This is the customers page.</p>
+    </div>
+  );
+};
+
+export default CustomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+const InvoicesPage = () => {
+  return (<div>
+    <h1>Invoices Page</h1>
+    <p>This is the invoices page.</p>
+  </div>)
+};
+export default InvoicesPage;
